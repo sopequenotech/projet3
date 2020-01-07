32c221e5821504f8dbfc038b335180678a418897
Kelly SOSSOE <kelly.sossoe@muggum.fr>
2020-01-07 16:44:18 +0100

    Update fonctions.php

--- php/fonctions.php
+++ php/fonctions.php
@@ -5,8 +5,8 @@
  * @return PDO Objet de connexion à la BD
  */
 function getBdd() {
-    return new PDO("mysql:host=localhost;dbname=gbaf-bdd;charset=utf8",
-        "root", "root",
+    return new PDO("mysql:host=localhost;dbname=projet3k_bdd;charset=utf8",
+        "projet3k_tech", "Chirac-234",
         array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 
