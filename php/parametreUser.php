d091b4610f7f6f4b8d76b6d5e5e8d65627e5d033
Kelly SOSSOE <kelly.sossoe@muggum.fr>
2020-02-08 18:43:28 +0100

    Update parametreUser.php

--- php/parametreUser.php
+++ php/parametreUser.php
@@ -31,7 +31,7 @@ $idUser = $_SESSION['idUser'];
 
 <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
